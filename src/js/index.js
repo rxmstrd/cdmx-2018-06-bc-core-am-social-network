4e17844597ca5f00c0d5a5dd9f0f36e4322537bb
Maria Rosa <ross.zhena@gmail.com>
2018-07-18 01:42:55 -0500

    Se agrega verificador de correo para login

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,15 @@ function registrar(){
   var password = document.getElementById('password').value;
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function () {
+    verificar()
+  })
   .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   });;
 }
 
@@ -26,11 +31,11 @@ function observador() {
   firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
       console.log("existe usuario activo")
-      aparece();
+      aparece(user);
     // User is signed in.
     var displayName = user.displayName;
     var email = user.email;
-    console.log(user);
+    console.log(user.emailVerified);
     var emailVerified = user.emailVerified;
     var photoURL = user.photoURL;
     var isAnonymous = user.isAnonymous;
@@ -44,11 +49,15 @@ function observador() {
 }
 observador();
 
-function aparece(){
+function aparece(user){
+  var user = user;
   var contenido = document.getElementById('contenido');
-  contenido.innerHTML =  `
-  <p>Bienvenido</p>
-  <button onclick="cerrar()">Cerrar sesión</button>`;
+  if(user.emailVerified){
+    contenido.innerHTML =  `
+    <p>Bienvenido</p>
+    <button onclick="cerrar()">Cerrar sesión</button>`;
+  }
+
 }
 
 function cerrar(){
@@ -59,4 +68,13 @@ function cerrar(){
   .catch(function(error){
     console.log(error)
   })
+}
+
+function verificar(){
+  var user = firebase.auth().currentUser;
+  user.sendEmailVerification().then(function() {
+  console.log("Enviando correo");
+}).catch(function(error) {
+console.log(error);
+});
 }
