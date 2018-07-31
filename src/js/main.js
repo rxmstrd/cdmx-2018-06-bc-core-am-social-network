4fded5309271134f15a3e8747366015bbf8eb0b9
rxmstrd <37351221+rxmstrd@users.noreply.github.com>
2018-07-31 14:21:02 -0500

    se agrega el maquetado del login

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,37 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const btnRegistrar = document.getElementById('registrar');
 const email2 = document.getElementById('email2');
 const password2 = document.getElementById('password2');
+
 const btnIngreso = document.getElementById('ingreso');
-const contenido = document.getElementById('contenido');
+const btnRegistrar = document.getElementById('registrar');
 const btnGoogle = document.getElementById('btn-google');
+
+const contenido = document.getElementById('contenido');
 
 
 btnRegistrar.addEventListener("click", event =>{
-window.social.registrar(email.value,password.value);
-//window.social.observador();
-
-}
+  window.social.registrar(email.value,password.value);
+    //window.social.observador();
+    document.querySelector('.modal-registro').classList.add('active');
+    document.querySelector('.loginGeneral').style.display = 'none';
+  }
 );
 
 btnIngreso.addEventListener("click", event =>{
-window.social.ingreso(email2.value,password2.value);
-//window.social.observador();
-
-}
+  window.social.ingreso(email2.value,password2.value);
+  //window.social.observador();
+  }
 );
 
 btnGoogle.addEventListener("click", event => {
-window.social.googleSignIn();
-//window.social.observador();
-}
+  window.social.googleSignIn();
+  //window.social.observador();
+  }
 );
+
+
+// const cerrarRegistro = () => {
+//   document.querySelector('.modal-registro').style.display = 'none';
+//   document.querySelector('.loginGeneral').style.display = 'inline-block';
+// }
